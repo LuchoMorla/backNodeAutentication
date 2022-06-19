c507ceee16c399fb1b8f0294469092e9e04839bd
morlucho <luis-fernando-10@outlook.es>
2022-06-18 23:54:34 -0500

    executing migracion productos

---
+++ db/migrations/20220619045115-products.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const { CategorySchema, CATEGORY_TABLE } = require('../../models/categoryModel');
+const { ProductSchema, PRODUCT_TABLE } = require('../../models/productModel');
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
